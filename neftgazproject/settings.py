30d8ee9785f8db01b5272c9e60b4cb0a82f8627d
AbrorjonDev <axmadov.git@gmail.com>
2022-02-26 00:56:07 +0500

    Project has been uploaded

--- neftgazproject/settings.py
+++ neftgazproject/settings.py
@@ -30,6 +30,15 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "abrorjonaxmadov.uz",
+    "localhost",
+    "*",
+]
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -50,8 +59,10 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'corsheaders.middleware.CorsPostCsrfMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
