b3efa37b43cc2313eb050858cb13a022d6d9bf71
AbrorjonDev <axmadov.git@gmail.com>
2022-03-06 20:44:35 +0500

    Viewsets added.

--- api/views.py
+++ api/views.py
@@ -8,222 +8,263 @@ from rest_framework.response import  Response
 # from rest_framework.
 
 
-class AdvertisementViews(views.APIView):
-    queryset = Advertisement.objects.all()
-    serializer_class = AdvertisementSerializer
-    permission_classes = (AllowAny,)
-    parser_classes = (parsers.JSONParser, parsers.MultiPartParser, parsers.FormParser)
-
-    def get(self, request):
-        serializer = self.serializer_class(Advertisement.objects.all(), many=True)
-        return Response(serializer.data, status=200)
-    
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=200)
-        return Response(serializer.errors, status=400)
-
-
-class AdvertisementsDetailView(views.APIView):
+class AdvertisementViews(viewsets.ModelViewSet):
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
     permission_classes = (AllowAny,)
     parser_classes = (parsers.JSONParser, parsers.MultiPartParser, parsers.FormParser)
 
 
-    def get(self, request, pk):
-        object = get_object_or_404(Advertisement.objects.all(), pk=pk)
-        serializer = self.serializer_class(object, many=False)
-        return Response(serializer.data, status=200)
-
-    def patch(self, request, pk):
-        object = get_object_or_404(Advertisement.objects.all(), pk=pk)
-        serializer = AdvertisementSerializer(object, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
-    
-    def delete(self, request, pk):
-        object = get_object_or_404(Advertisement.objects.all(), pk=pk)
-        object.delete()
-        return Response({'detail':'ok'})
-
-    
-# class AdvertisementsDetailView(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Advertisement.objects.all()
-#     lookup_url_kwarg = 'pk'
-#     serializer_class = AdvertisementSerializer
-#     permission_classes = (AllowAny,)
-
-class NewsViews(views.APIView):
+class NewsViews(viewsets.ModelViewSet):
     queryset = News.objects.all()
     serializer_class = NewsSerializer
     permission_classes = (AllowAny,)
     parser_classes = (parsers.JSONParser, parsers.MultiPartParser, parsers.FormParser)
 
-    def get(self, request):
-        serializer = self.serializer_class(News.objects.all(), many=True)
-        return Response(serializer.data, status=200)
-    
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=200)
-        return Response(serializer.errors, status=400)
 
 
-class NewsDetailView(views.APIView):
-    queryset = News.objects.all()
-    serializer_class = NewsSerializer
-    permission_classes = (AllowAny,)
-    parser_classes = (parsers.JSONParser, parsers.MultiPartParser, parsers.FormParser)
-
-    def get(self, request, pk):
-        object = get_object_or_404(News.objects.all(), pk=pk)
-        serializer = self.serializer_class(object, many=False)
-        return Response(serializer.data, status=200)
-
-    def patch(self, request, pk):
-        object = get_object_or_404(News.objects.all(), pk=pk)
-        serializer = NewsSerializer(object, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
-    
-    def delete(self, request, pk):
-        object = get_object_or_404(News.objects.all(), pk=pk)
-        object.delete()
-        return Response({'detail':'ok'})
-
-
-class AccountantsViews(views.APIView):
+class AccountantsViews(viewsets.ModelViewSet):
     queryset = Accountants.objects.all()
     serializer_class = AccountantsSerializer
     permission_classes = (AllowAny,)
     parser_classes = (parsers.JSONParser, parsers.MultiPartParser, parsers.FormParser)
 
-    def get(self, request):
-        serializer = self.serializer_class(Accountants.objects.all(), many=True)
-        return Response(serializer.data, status=200)
-    
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=200)
-        return Response(serializer.errors, status=400)
 
-
-class AccountantsDetailView(views.APIView):
-    queryset = Accountants.objects.all()
-    serializer_class = AccountantsSerializer
-    permission_classes = (AllowAny,)
-    parser_classes = (parsers.JSONParser, parsers.MultiPartParser, parsers.FormParser)
-
-    def get(self, request, pk):
-        object = get_object_or_404(Accountants.objects.all(), pk=pk)
-        serializer = self.serializer_class(object, many=False)
-        return Response(serializer.data, status=200)
-
-    def patch(self, request, pk):
-        object = get_object_or_404(Accountants.objects.all(), pk=pk)
-        serializer = AccountantsSerializer(object, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
-    
-    def delete(self, request, pk):
-        object = get_object_or_404(Accountants.objects.all(), pk=pk)
-        object.delete()
-        return Response({'detail':'ok'})
-
-
-class TendersViews(views.APIView):
+class TendersViews(viewsets.ModelViewSet):
     queryset = Tenders.objects.all()
     serializer_class = TendersSerializer
     permission_classes = (AllowAny,)
     parser_classes = (parsers.JSONParser, parsers.MultiPartParser, parsers.FormParser)
 
-    def get(self, request):
-        serializer = self.serializer_class(Tenders.objects.all(), many=True)
-        return Response(serializer.data, status=200)
-    
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=200)
-        return Response(serializer.errors, status=400)
 
 
-class TendersDetailView(views.APIView):
-    queryset = Tenders.objects.all()
-    serializer_class = TendersSerializer
-    permission_classes = (AllowAny,)
-    parser_classes = (parsers.JSONParser, parsers.MultiPartParser, parsers.FormParser)
 
-    def get(self, request, pk):
-        object = get_object_or_404(Tenders.objects.all(), pk=pk)
-        serializer = self.serializer_class(object, many=False)
-        return Response(serializer.data, status=200)
-
-    def patch(self, request, pk):
-        object = get_object_or_404(Tenders.objects.all(), pk=pk)
-        serializer = TendersSerializer(object, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
-    
-    def delete(self, request, pk):
-        object = get_object_or_404(Tenders.objects.all(), pk=pk)
-        object.delete()
-        return Response({'detail':'ok'})
-
-class NounsViews(views.APIView):
+class NounsViews(viewsets.ModelViewSet):
     queryset = Nouns.objects.all()
     serializer_class =NounsSerializer
     permission_classes = (AllowAny,)
     parser_classes = (parsers.JSONParser, parsers.MultiPartParser, parsers.FormParser)
 
-    def get(self, request):
-        serializer = self.serializer_class(Nouns.objects.all(), many=True)
-        return Response(serializer.data, status=200)
+
+
+
+
+# class AdvertisementViews(views.APIView):
+#     queryset = Advertisement.objects.all()
+#     serializer_class = AdvertisementSerializer
+#     permission_classes = (AllowAny,)
+#     parser_classes = (parsers.JSONParser, parsers.MultiPartParser, parsers.FormParser)
+
+#     def get(self, request):
+#         serializer = self.serializer_class(Advertisement.objects.all(), many=True)
+#         return Response(serializer.data, status=200)
     
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=200)
-        return Response(serializer.errors, status=400)
+#     def post(self, request, *args, **kwargs):
+#         serializer = self.serializer_class(data=request.data)
+#         if serializer.is_valid(raise_exception=True):
+#             serializer.save()
+#             return Response(serializer.data, status=200)
+#         return Response(serializer.errors, status=400)
 
 
-class NounsDetailView(views.APIView):
-    queryset = Nouns.objects.all()
-    serializer_class = NounsSerializer
-    permission_classes = (AllowAny,)
-    parser_classes = (parsers.JSONParser, parsers.MultiPartParser, parsers.FormParser)
+# class AdvertisementsDetailView(views.APIView):
+#     queryset = Advertisement.objects.all()
+#     serializer_class = AdvertisementSerializer
+#     permission_classes = (AllowAny,)
+#     parser_classes = (parsers.JSONParser, parsers.MultiPartParser, parsers.FormParser)
 
-    def get(self, request, pk):
-        object = get_object_or_404(Nouns.objects.all(), pk=pk)
-        serializer = self.serializer_class(object, many=False)
-        return Response(serializer.data, status=200)
 
-    def patch(self, request, pk):
-        object = get_object_or_404(Nouns.objects.all(), pk=pk)
-        serializer = NounsSerializer(object, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+#     def get(self, request, pk):
+#         object = get_object_or_404(Advertisement.objects.all(), pk=pk)
+#         serializer = self.serializer_class(object, many=False)
+#         return Response(serializer.data, status=200)
+
+#     def patch(self, request, pk):
+#         object = get_object_or_404(Advertisement.objects.all(), pk=pk)
+#         serializer = AdvertisementSerializer(object, data=request.data, partial=True)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors)
     
-    def delete(self, request, pk):
-        object = get_object_or_404(Nouns.objects.all(), pk=pk)
-        object.delete()
-        return Response({'detail':'ok'})
+#     def delete(self, request, pk):
+#         object = get_object_or_404(Advertisement.objects.all(), pk=pk)
+#         object.delete()
+#         return Response({'detail':'ok'})
+
+    
+# # class AdvertisementsDetailView(generics.RetrieveUpdateDestroyAPIView):
+# #     queryset = Advertisement.objects.all()
+# #     lookup_url_kwarg = 'pk'
+# #     serializer_class = AdvertisementSerializer
+# #     permission_classes = (AllowAny,)
+
+# class NewsViews(views.APIView):
+#     queryset = News.objects.all()
+#     serializer_class = NewsSerializer
+#     permission_classes = (AllowAny,)
+#     parser_classes = (parsers.JSONParser, parsers.MultiPartParser, parsers.FormParser)
+
+#     def get(self, request):
+#         serializer = self.serializer_class(News.objects.all(), many=True)
+#         return Response(serializer.data, status=200)
+    
+#     def post(self, request, *args, **kwargs):
+#         serializer = self.serializer_class(data=request.data)
+#         if serializer.is_valid(raise_exception=True):
+#             serializer.save()
+#             return Response(serializer.data, status=200)
+#         return Response(serializer.errors, status=400)
+
+
+# class NewsDetailView(views.APIView):
+#     queryset = News.objects.all()
+#     serializer_class = NewsSerializer
+#     permission_classes = (AllowAny,)
+#     parser_classes = (parsers.JSONParser, parsers.MultiPartParser, parsers.FormParser)
+
+#     def get(self, request, pk):
+#         object = get_object_or_404(News.objects.all(), pk=pk)
+#         serializer = self.serializer_class(object, many=False)
+#         return Response(serializer.data, status=200)
+
+#     def patch(self, request, pk):
+#         object = get_object_or_404(News.objects.all(), pk=pk)
+#         serializer = NewsSerializer(object, data=request.data, partial=True)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors)
+    
+#     def delete(self, request, pk):
+#         object = get_object_or_404(News.objects.all(), pk=pk)
+#         object.delete()
+#         return Response({'detail':'ok'})
+
+
+# class AccountantsViews(views.APIView):
+#     queryset = Accountants.objects.all()
+#     serializer_class = AccountantsSerializer
+#     permission_classes = (AllowAny,)
+#     parser_classes = (parsers.JSONParser, parsers.MultiPartParser, parsers.FormParser)
+
+#     def get(self, request):
+#         serializer = self.serializer_class(Accountants.objects.all(), many=True)
+#         return Response(serializer.data, status=200)
+    
+#     def post(self, request, *args, **kwargs):
+#         serializer = self.serializer_class(data=request.data)
+#         if serializer.is_valid(raise_exception=True):
+#             serializer.save()
+#             return Response(serializer.data, status=200)
+#         return Response(serializer.errors, status=400)
+
+
+# class AccountantsDetailView(views.APIView):
+#     queryset = Accountants.objects.all()
+#     serializer_class = AccountantsSerializer
+#     permission_classes = (AllowAny,)
+#     parser_classes = (parsers.JSONParser, parsers.MultiPartParser, parsers.FormParser)
+
+#     def get(self, request, pk):
+#         object = get_object_or_404(Accountants.objects.all(), pk=pk)
+#         serializer = self.serializer_class(object, many=False)
+#         return Response(serializer.data, status=200)
+
+#     def patch(self, request, pk):
+#         object = get_object_or_404(Accountants.objects.all(), pk=pk)
+#         serializer = AccountantsSerializer(object, data=request.data, partial=True)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors)
+    
+#     def delete(self, request, pk):
+#         object = get_object_or_404(Accountants.objects.all(), pk=pk)
+#         object.delete()
+#         return Response({'detail':'ok'})
+
+
+# class TendersViews(views.APIView):
+#     queryset = Tenders.objects.all()
+#     serializer_class = TendersSerializer
+#     permission_classes = (AllowAny,)
+#     parser_classes = (parsers.JSONParser, parsers.MultiPartParser, parsers.FormParser)
+
+#     def get(self, request):
+#         serializer = self.serializer_class(Tenders.objects.all(), many=True)
+#         return Response(serializer.data, status=200)
+    
+#     def post(self, request, *args, **kwargs):
+#         serializer = self.serializer_class(data=request.data)
+#         if serializer.is_valid(raise_exception=True):
+#             serializer.save()
+#             return Response(serializer.data, status=200)
+#         return Response(serializer.errors, status=400)
+
+
+# class TendersDetailView(views.APIView):
+#     queryset = Tenders.objects.all()
+#     serializer_class = TendersSerializer
+#     permission_classes = (AllowAny,)
+#     parser_classes = (parsers.JSONParser, parsers.MultiPartParser, parsers.FormParser)
+
+#     def get(self, request, pk):
+#         object = get_object_or_404(Tenders.objects.all(), pk=pk)
+#         serializer = self.serializer_class(object, many=False)
+#         return Response(serializer.data, status=200)
+
+#     def patch(self, request, pk):
+#         object = get_object_or_404(Tenders.objects.all(), pk=pk)
+#         serializer = TendersSerializer(object, data=request.data, partial=True)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors)
+    
+#     def delete(self, request, pk):
+#         object = get_object_or_404(Tenders.objects.all(), pk=pk)
+#         object.delete()
+#         return Response({'detail':'ok'})
+
+# class NounsViews(views.APIView):
+#     queryset = Nouns.objects.all()
+#     serializer_class =NounsSerializer
+#     permission_classes = (AllowAny,)
+#     parser_classes = (parsers.JSONParser, parsers.MultiPartParser, parsers.FormParser)
+
+#     def get(self, request):
+#         serializer = self.serializer_class(Nouns.objects.all(), many=True)
+#         return Response(serializer.data, status=200)
+    
+#     def post(self, request, *args, **kwargs):
+#         serializer = self.serializer_class(data=request.data)
+#         if serializer.is_valid(raise_exception=True):
+#             serializer.save()
+#             return Response(serializer.data, status=200)
+#         return Response(serializer.errors, status=400)
+
+
+# class NounsDetailView(views.APIView):
+#     queryset = Nouns.objects.all()
+#     serializer_class = NounsSerializer
+#     permission_classes = (AllowAny,)
+#     parser_classes = (parsers.JSONParser, parsers.MultiPartParser, parsers.FormParser)
+
+#     def get(self, request, pk):
+#         object = get_object_or_404(Nouns.objects.all(), pk=pk)
+#         serializer = self.serializer_class(object, many=False)
+#         return Response(serializer.data, status=200)
+
+#     def patch(self, request, pk):
+#         object = get_object_or_404(Nouns.objects.all(), pk=pk)
+#         serializer = NounsSerializer(object, data=request.data, partial=True)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors)
+    
+#     def delete(self, request, pk):
+#         object = get_object_or_404(Nouns.objects.all(), pk=pk)
+#         object.delete()
+#         return Response({'detail':'ok'})
--- neftgazproject/urls.py
+++ neftgazproject/urls.py
@@ -34,41 +34,41 @@ from api.views import *
 router = DefaultRouter()
 
 
-# router.register(r'advertisement', AdvertisementViewSet)
-# router.register(r'news', NewsViewSet)
-# router.register(r'accountant', AccountantsViewSet)
-# router.register(r'tenders', TendersViewSet)
-# router.register(r'noun', NounsViewSet)
+router.register(r'advertisement', AdvertisementViews)
+router.register(r'news', NewsViews)
+router.register(r'accountant', AccountantsViews)
+router.register(r'tenders', TendersViews)
+router.register(r'noun', NounsViews)
 
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'advertisement':'https://neft-gaz.api.abrorjonaxmadov.uz/advertisement/',
-        'news':'https://neft-gaz.api.abrorjonaxmadov.uz/news/',
-        'accountant':'https://neft-gaz.api.abrorjonaxmadov.uz/accountant/',
-        'tenders':'https://neft-gaz.api.abrorjonaxmadov.uz/tenders/',
-        'noun':'https://neft-gaz.api.abrorjonaxmadov.uz/noun/',
-    })
+# @api_view(['GET'])
+# def api_root(request, format=None):
+#     return Response({
+#         'advertisement':'https://neft-gaz.api.abrorjonaxmadov.uz/advertisement/',
+#         'news':'https://neft-gaz.api.abrorjonaxmadov.uz/news/',
+#         'accountant':'https://neft-gaz.api.abrorjonaxmadov.uz/accountant/',
+#         'tenders':'https://neft-gaz.api.abrorjonaxmadov.uz/tenders/',
+#         'noun':'https://neft-gaz.api.abrorjonaxmadov.uz/noun/',
+#     })
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', api_root, name='api_root'),
-    # path('add/<int:pk>', AdvertisementViews.as_view(), name="addvertisements-detail"),
+    # path('', api_root, name='api_root'),
+    # # path('add/<int:pk>', AdvertisementViews.as_view(), name="addvertisements-detail"),
 
-    path('advertisement/', AdvertisementViews.as_view(), name="addvertisements"),
-    path(r'advertisement/<int:pk>/', AdvertisementsDetailView.as_view(), name="addvertisements-detail"),
+    # path('advertisement/', AdvertisementViews.as_view(), name="addvertisements"),
+    # path(r'advertisement/<int:pk>/', AdvertisementsDetailView.as_view(), name="addvertisements-detail"),
 
-    path('news/', NewsViews.as_view(), name="news"),
-    path(r'news/<int:pk>/', NewsDetailView.as_view(), name="addvertisements-detail"),
+    # path('news/', NewsViews.as_view(), name="news"),
+    # path(r'news/<int:pk>/', NewsDetailView.as_view(), name="addvertisements-detail"),
 
-    path('accountant/', AccountantsViews.as_view(), name="addvertisements"),
-    path(r'accountant/<int:pk>/', AccountantsDetailView.as_view(), name="addvertisements-detail"),
+    # path('accountant/', AccountantsViews.as_view(), name="addvertisements"),
+    # path(r'accountant/<int:pk>/', AccountantsDetailView.as_view(), name="addvertisements-detail"),
 
-    path('tenders/', TendersViews.as_view(), name="addvertisements"),
-    path(r'tenders/<int:pk>/', TendersDetailView.as_view(), name="addvertisements-detail"),
+    # path('tenders/', TendersViews.as_view(), name="addvertisements"),
+    # path(r'tenders/<int:pk>/', TendersDetailView.as_view(), name="addvertisements-detail"),
 
-    path('noun/', NounsViews.as_view(), name="addvertisements"),
-    path(r'noun/<int:pk>/', NounsDetailView.as_view(), name="addvertisements-detail"),
+    # path('noun/', NounsViews.as_view(), name="addvertisements"),
+    # path(r'noun/<int:pk>/', NounsDetailView.as_view(), name="addvertisements-detail"),
 
 
 
